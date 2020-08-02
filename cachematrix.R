0f7d2f51e7b60085c9cdd6bd8dd99fa10d46d9e1
devonjwilton <62908801+devonjwilton@users.noreply.github.com>
2020-08-02 18:49:36 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,11 @@
 ## makeCacheMatrix is a function that initializes two objects (x and inv) and creates four functions (set, get, setinv, getinv).
 ## Set allows the matrix being used to calculate the inverse to be changed without reinitializing the makeCacheMatrix function by
-## passing the argument for set (y) to the parent environment and setting "x" to this value. It also resets the inverse to NULL so that
-## when the matrix is changed any inverse value that has been calculated is also reset. Get and getinv return the matrix and matrix inverse, respectively,
-## and setinv sets the inverse matrix using an argument passed from cacheSolve.
-## CacheSolve takes the object containing makeCacheMatrix as an input and checks to see if an inverse has been calculated for the matrix
-## stored as x in makeCacheMatrix. If an inverse is already stored cacheSolve says that it's retrieving the cached data and returns
-## the inverse matrix. If no inverse matrix has yet been calculated, cacheSolve solves for the inverse matrix and then sets the inverse
-## using setinv, so that next time the inverse is called (assuming the matrix has not been changed), it does not need to be calculated again.
+## passing the argument for set (y) to the parent environment using <<- and setting "x" to this value. It also resets the inverse (inv) to NULL in the parent environment
+## using the same <<- operator so that when the matrix is changed using set(), any inverse value that has been calculated is also reset. Get and getinv return the matrix
+## and matrix inverse, respectively, and setinv sets the inverse matrix using an argument passed from cacheSolve.
+
 ##Line by line descriptions included in the functions below as well.
+
 
 makeCacheMatrix <- function(x = matrix()) {     #Create function makeCacheMatrix, initialize x as an object (1x1 matrix, value NA) within the function argument.
     inv <- NULL                                 #initialize inv as an object set to null
@@ -26,6 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {     #Create function makeCacheMatrix
          setinv = setinv,                       #This allows the functions to be called by name using the $ operator.
          getinv = getinv)
 }
+
+## CacheSolve takes the object containing makeCacheMatrix as an input and checks to see if an inverse has been calculated for the matrix
+## stored as x in makeCacheMatrix. If an inverse is already stored cacheSolve says that it's retrieving the cached data and returns
+## the inverse matrix. If no inverse matrix has yet been calculated, cacheSolve solves for the inverse matrix and then sets the inverse
+## using setinv, so that next time the inverse is called (assuming the matrix has not been changed), it does not need to be calculated again.
 
 cacheSolve <- function(x, ...) {                #Create function cacheSolve with input x; note that "x" should be whatever object makeCacheMatrix was assigned to.
     inv <- x$getinv()                           #Attempts to retrieve the inverse from the object passed in the cacheSolve argument using the getinv function.
